35f6cc03898912b87ac254143d24f3f08cacea23
camdenb <doctorshmoogle@gmail.com>
2015-03-01 12:38:04 -0800

    added ball tracking and best base

--- main.lua
+++ main.lua
@@ -8,19 +8,17 @@ local runnerSize = 10
 
 local baseline = vector(100, 40)
 local mound = vector(200, 200)
-local bases = {
-	vector(0, 0),
-	vector(0, 0),
-	vector(0, 0),
-	vector(0, 0),
-}
+local bases = nil
 
 local baseRunners = {
-	{active = true, totalDistance = 0},
-	{active = false, totalDistance = 20},
-	{active = false, totalDistance = 20},
-	{active = false, totalDistance = 20}
+	{active = true, totalDistance = 0, speed = 1},
+	{active = false, totalDistance = 110, speed = 1},
+	{active = true, totalDistance = 290, speed = 1},
+	{active = false, totalDistance = 20, speed = 1}
 }
+local ball = {pos = vector(0,0), speed = vector(0,0)}
+
+local fielder = {pos = vector(240, 400), throwingSpeed = 7}
 
 
 
@@ -28,7 +26,7 @@ local paused = true
 local cam = nil
 
 local tickCounter = 0
-local tickMax = 1
+local tickMax = 3
 
 function love.load()
 
@@ -44,10 +42,10 @@ function love.load()
 	mound.x = WINDOW_WIDTH / 2 - baseSize / 2
 	mound.y = WINDOW_HEIGHT / 2 - baseSize / 2
 	bases = {
-		vector(mound.x + baseDistanceScale, mound.y),
-		vector(mound.x, mound.y - baseDistanceScale),
-		vector(mound.x - baseDistanceScale, mound.y),
-		vector(mound.x, mound.y + baseDistanceScale),
+		{pos = vector(mound.x + baseDistanceScale, mound.y), distance = 90},
+		{pos = vector(mound.x, mound.y - baseDistanceScale), distance = 180},
+		{pos = vector(mound.x - baseDistanceScale, mound.y), distance = 270},
+		{pos = vector(mound.x, mound.y + baseDistanceScale), distance = 360}
 	}	
 
 	-- for i,v in ipairs(bases) do
@@ -72,7 +70,14 @@ end
 
 function tick()
 	if not paused then
-		-- baseRunners[1].totalDistance = baseRunners[1].totalDistance + 1
+		for i,v in ipairs(baseRunners) do
+			if v.active then
+				v.totalDistance = v.totalDistance + v.speed
+			end
+		end
+
+		ball.pos = ball.pos + ball.speed
+
 	end
 end
 
@@ -94,22 +99,42 @@ function drawWorld()
 	love.graphics.setLineWidth(baseSize)
 	--love.graphics.rectangle('fill', baseline.x, baseline.y, 10, 360)
 	for i = 1, 4, 1 do
-		love.graphics.line(bases[i].x + baseSize / 2, bases[i].y + baseSize / 2, bases[(i) % 4 + 1].x + baseSize / 2, bases[(i) % 4 + 1].y + baseSize / 2)
+		love.graphics.line(bases[i].pos.x + baseSize / 2, bases[i].pos.y + baseSize / 2, bases[(i) % 4 + 1].pos.x + 
+			baseSize / 2, bases[(i) % 4 + 1].pos.y + baseSize / 2)
 	end
 
 	love.graphics.setColor(240, 240, 240)
 	for i,v in ipairs(bases) do
-		love.graphics.rectangle('fill', v.x, v.y, baseSize, baseSize)
+		love.graphics.rectangle('fill', v.pos.x, v.pos.y, baseSize, baseSize)
 	end
 	love.graphics.rectangle('fill', mound.x, mound.y + baseSize / 4, baseSize, baseSize / 2)
 
-	love.graphics.setColor(100, 200, 200)
 	for i,v in ipairs(baseRunners) do
 		if v.active then
 			local newX, newY = getPositionFromTotalDistance(v.totalDistance)
+			love.graphics.setColor(100, 200, 200)
 			love.graphics.rectangle('fill', newX, newY, runnerSize, runnerSize)
+			-- love.graphics.setColor(255, 0, 0, 10)
+			-- love.graphics.setLineWidth(1)
+			-- for i2,v2 in ipairs(bases) do
+			-- 	love.graphics.line(newX, newY, v2.x, v2.y)
+			-- end
 		end
 	end
+
+	love.graphics.setColor(255, 0, 0, 50)
+	love.graphics.setLineWidth(1)
+	for i2,v2 in ipairs(baseRunners) do
+		local b = getBaseToThrowTo(fielder.pos, fielder.throwingSpeed)
+		love.graphics.line(fielder.pos.x, fielder.pos.y, b.pos.x, b.pos.y)
+	end
+
+	love.graphics.setColor(200, 255, 200)
+	love.graphics.rectangle('fill', fielder.pos.x, fielder.pos.y, runnerSize, runnerSize)
+	love.graphics.line(ball.pos.x, ball.pos.y, ball.pos.x + (ball.speed.x) * 10, ball.pos.y + (ball.speed.y) * 10)
+
+	love.graphics.setColor(100, 255, 255)
+	love.graphics.rectangle('fill', ball.pos.x, ball.pos.y, 5, 5)
 
 end
 
@@ -122,6 +147,10 @@ function love.keypressed(key)
 
 	if key == 'p' or key == 'escape' then
 		paused = not paused
+	elseif key == 'n' then
+		table.insert(baseRunners, {active = true, totalDistance = 0})
+	elseif key == 't' then
+		throwBall(fielder)
 	end
 
 end
@@ -131,25 +160,66 @@ function getPositionFromTotalDistance(dist)
 
 	if dist >= 0 and dist < 90 then
 		percentDist = (dist % 90) / 90
-		pos.x = bases[4].x + percentDist * (bases[1].x - bases[4].x)
-		pos.y = bases[4].y + percentDist * (bases[1].y - bases[4].y)
+		pos.x = bases[4].pos.x + percentDist * (bases[1].pos.x - bases[4].pos.x) + runnerSize / 2
+		pos.y = bases[4].pos.y + percentDist * (bases[1].pos.y - bases[4].pos.y) + runnerSize / 2
 	elseif dist >= 90 and dist < 180 then
 		percentDist = (dist % 90) / 90
-		pos.x = bases[1].x - percentDist * math.abs(bases[1].x - bases[2].x)
-		pos.y = bases[1].y - percentDist * math.abs(bases[1].y - bases[2].y)
-		print(pos)
+		pos.x = bases[1].pos.x - percentDist * math.abs(bases[1].pos.x - bases[2].pos.x)
+		pos.y = bases[1].pos.y - percentDist * math.abs(bases[1].pos.y - bases[2].pos.y)
 	elseif dist >= 180 and dist < 270 then
 		percentDist = (dist % 90) / 90
-		pos.x = bases[2].x - percentDist * math.abs(bases[3].x - bases[2].x)
-		pos.y = bases[2].y + percentDist * math.abs(bases[3].y - bases[2].y)
+		pos.x = bases[2].pos.x - percentDist * math.abs(bases[3].pos.x - bases[2].pos.x) + runnerSize / 2
+		pos.y = bases[2].pos.y + percentDist * math.abs(bases[3].pos.y - bases[2].pos.y) + runnerSize / 2
 	elseif dist >= 270 and dist <= 360 then
 		percentDist = (dist % 90) / 90
-		pos.x = bases[3].x + percentDist * math.abs(bases[4].x - bases[3].x)
-		pos.y = bases[3].y + percentDist * math.abs(bases[4].y - bases[3].y)
+		pos.x = bases[3].pos.x + percentDist * math.abs(bases[4].pos.x - bases[3].pos.x)
+		pos.y = bases[3].pos.y + percentDist * math.abs(bases[4].pos.y - bases[3].pos.y)
 	else
-		pos.x = bases[4].x
-		pos.y = bases[4].y
+		pos.x = bases[4].pos.x
+		pos.y = bases[4].pos.y
 	end
 
 	return pos.x, pos.y
+end
+
+function getBaseToThrowTo(fielderPos, throwSpeed)
+
+	local bestBase = nil
+	local bestTime = 10000
+
+	for i_base, base in ipairs(bases) do
+		local throwTime = fielderPos:dist(base.pos) / throwSpeed
+		for i_runner, runner in ipairs(baseRunners) do
+			if runner.active then
+				local runTime = (base.distance - runner.totalDistance) / runner.speed
+				-- print('Time for runner', runTime)
+				-- print('Time for thrower', throwTime)
+				-- print('runnerTime - ballTime', (runTime - throwTime), runner.totalDistance)
+				if (runTime - throwTime) < bestTime and (runTime - throwTime) > 0 then
+					if bestBase == nil then
+						bestBase = base
+					end
+					if base.distance >= bestBase.distance then
+						bestBase = base
+						bestTime = (runTime - throwTime)
+					end
+				end
+			end
+		end
+	end
+
+	if bestBase == nil then
+		bestBase = bases[4]
+	end
+
+	return bestBase
+
+end
+
+function throwBall()
+	ball.pos = fielder.pos
+	bestBase = getBaseToThrowTo(fielder.pos, fielder.throwingSpeed)
+	ball.speed = bestBase.pos - ball.pos
+	ball.speed = ball.speed:normalized() * fielder.throwingSpeed
+	print(ball.speed)
 end
